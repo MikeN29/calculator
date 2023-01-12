8e9926b8f6de7f703df92c9a96c9a33c7ed98e2e
Michael Norman <mikenorman192@msn.com>
2023-01-12 15:01:48 +0000

    starting to add logic

--- script.js
+++ script.js
@@ -27,3 +27,39 @@ function operate(num1, num2, operator) {
     };
     
 
+let displayValue = '';
+
+const nine = document.getElementById('nine');
+const eight = document.getElementById('eight');
+const seven = document.getElementById('seven');
+const six = document.getElementById('six');
+const five = document.getElementById('five');
+const four = document.getElementById('four');
+const three = document.getElementById('three');
+const two = document.getElementById('two');
+const one = document.getElementById('one');
+const zero = document.getElementById('zero');
+
+nine.addEventListener('click', populateDisplay(9));
+eight.addEventListener('click', populateDisplay(8))
+
+
+
+function populateDisplay(num) {
+    displayValue.innerHTML += num;
+    console.log(num);
+
+
+
+
+
+
+    
+
+
+}
+
+
+
+// connect the number buttons to a function
+// this could mean appending them to a string? and this string being displayed as they are clicked?
